58af825c47a66656b8f6701b3eaef1599e942f0b
Jim Cosgrave <jim.cosgrave.web@gmail.com>
2020-04-16 22:38:30 -0500

    Added automatic API toggle on deploy

--- src/components/Shared/Environment.jsx
+++ src/components/Shared/Environment.jsx
@@ -1,5 +1,10 @@
 const env = {};
-//env.apiPrefix = 'https://grocery-api.now.sh/';
-env.apiPrefix = 'http://localhost:3100/';
+
+if(process.env.NODE_ENV === 'production') {
+    env.apiPrefix = 'https://grocery-api.now.sh/';
+} else {
+    env.apiPrefix = 'http://localhost:3100/';
+}
+
 
 export default env;
