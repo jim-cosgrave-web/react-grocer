954d0f8733396f4508f013e7dfaee506602dc5c1
Jim Cosgrave <jcosgrave@bdo.com>
2020-05-07 07:36:38 -0500

    Updated fa icons

--- src/components/Recipes/Recipe.jsx
+++ src/components/Recipes/Recipe.jsx
@@ -5,19 +5,46 @@ import env from '../Shared/Environment';
 import { Link, useParams } from "react-router-dom";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
 
 const Recipe = (props) => {
     const { recipeId } = useParams();
     const [recipe, setRecipe] = useState(null);
+    const [addGroceryWorkflow, setAddGroceryWorkflow] = useState({ step: 'initial' })
 
     useEffect(() => {
         const user_id = localStorage.getItem('user_id');
 
         axios.get(env.apiPrefix + 'recipes/' + user_id + '/' + recipeId).then(res => {
+            const recipe = res.data;
+
+            for(let i = 0; i < recipe.ingredients.length; i++) {
+                recipe.ingredients[i].checked = false;
+            }
+
             setRecipe(res.data);
         });
     }, []);
+
+    const addToList = () => {
+        const clone = { ...addGroceryWorkflow };
+        clone.step = 'confirmation';
+        setAddGroceryWorkflow(clone);
+    }
+
+    const cancel = () => {
+        const clone = { ...addGroceryWorkflow };
+        clone.step = 'initial';
+        setAddGroceryWorkflow(clone);
+    }
+
+    const toggleIngredientChecked = (ingredient) => {
+        const clone = { ...recipe };
+        const index = clone.ingredients.indexOf(ingredient);
+        clone.ingredients[index].checked = !clone.ingredients[index].checked;
+        setRecipe(clone);
+    }
 
     let content = (
         <div style={{ maxWidth: "600px" }}>
@@ -31,13 +58,24 @@ const Recipe = (props) => {
                 {recipe.link && <div>
                     <a target="_blank" href={recipe.link}>Recipe Link</a>
                 </div>}
-                {recipe.ingredients && recipe.ingredients.length > 0 && <div>
+                {recipe.ingredients && recipe.ingredients.length > 0 && <div className="edit-recipe-container">
                     <div className="recipe-section-title">Ingredients</div>
-                    {recipe.ingredients.map((i, index) => {
-                        return (
-                            <div key={i.name}>{i.name}</div>
-                        );
-                    })}
+                    <div className="edit-list-container" style={{ marginTop: "10px" }}>
+                        {recipe.ingredients.map((i, index) => {
+                            return (
+                                <div key={i.name} className="edit-list">
+                                    <div>
+                                        {i.name}
+                                    </div>
+                                    <div  style={{ marginRight: "6px" }}>
+                                    {i.checked && <FontAwesomeIcon className="clickable" icon={faCheckSquare} onClick={() => toggleIngredientChecked(i)} />}
+                                    {!i.checked && <FontAwesomeIcon className="clickable" icon={faSquare} onClick={() => toggleIngredientChecked(i)} />}
+                                    </div>
+                                </div>
+
+                            );
+                        })}
+                    </div>
                 </div>}
                 {recipe.categories && recipe.categories.length > 0 && <div>
                     <div className="recipe-section-title">Categories</div>
@@ -47,6 +85,18 @@ const Recipe = (props) => {
                         );
                     })}
                 </div>}
+                <div style={{ marginTop: "10px" }}>
+                    {addGroceryWorkflow.step == 'initial' && <button className="save" style={{ width: "100%" }} onClick={addToList}>
+                        Add To List
+                    </button>}
+                    {addGroceryWorkflow.step == 'confirmation' && <div>
+                        <div className="list-btn-container">
+                            <div className="g-btn g-btn-large btn-hide btn-warning noselect">Add to List</div>
+                            <div onClick={cancel} className="g-btn g-btn-large btn-clear btn-danger noselect">Cancel</div>
+                        </div>
+                    </div>
+                    }
+                </div>
             </div>}
             {!recipe && <div>
                 Loading...
